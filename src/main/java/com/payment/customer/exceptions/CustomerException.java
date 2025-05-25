4de07da8ce233e1f9f6de0eedc9d985c369f7b1f
josebarrontovar <josebarrontovar@hotmail.com>
2025-05-25 16:17:40 -0600

    Refact

--- src/main/java/com/payment/customer/exceptions/CustomerException.java
+++
@@ -1,7 +0,0 @@
-package com.payment.customer.exceptions;
-
-public class CustomerException extends RuntimeException{
-    public CustomerException(String message) {
-        super(message);
-    }
-}
